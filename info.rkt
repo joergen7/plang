328eb5925173ff8d710689ad64c110519e2a4005
Jorgen Brandt <joergen@cuneiform-lang.org>
2019-03-21 23:28:48 +0100

    package scaffold

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "plang")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/plang.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(jorgen))
---
+++ scribblings/plang.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[plang
+                    racket/base]]
+
+@title{plang}
+@author{jorgen}
+
+@defmodule[plang]
+
+Package Description Here
